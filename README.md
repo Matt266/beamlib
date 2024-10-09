3122f4c4fca7f1fa866864e783efeeab7e53a36b
scm32482 <matthias.schoen@st.oth-regensburg.de>
2024-10-09 19:50:37 +0200

    added README.md and steering over wavenumber axis

---
+++ README.md
@@ -0,0 +1,16 @@
+# BeamLib.jl
+BeamLib is a Julia package for the evaluation of various beamformers. 
+
+While some other related packages do exist:
+
+    - JuliaGNSS/PhasedArray.jl (https://github.com/JuliaGNSS/PhasedArray.jl)
+    - Beamforming.jl (https://juliapackages.com/p/beamforming)
+    - SignalAnalysis.jl (https://github.com/org-arl/SignalAnalysis.jl)
+
+None of them are focused on simulation and evalution, support Unitful for usage of physical units, or
+allow for deep customization.
+
+New functionality will be added to this package as needed.
+
+## Examples
+Jupyter Notebooks with examples can be found in the examples directory
--- src/BeamLib.jl
+++ src/BeamLib.jl
@@ -37,7 +37,7 @@ PhasedArray3D(x::PhasedArray2D{T})  where {T <: Number} = PhasedArray3D{T}([(e[1
 Base.convert(::Type{PhasedArray3D}, x::PhasedArray1D{T}) where {T <: Number} = PhasedArray3D(x)
 Base.convert(::Type{PhasedArray3D}, x::PhasedArray2D{T}) where {T <: Number} = PhasedArray3D(x)
 
-function steerphi(x::PhasedArray1D, f, ϕ; fs=nothing, c=c_0, direction::WaveDirection=Incoming)
+function steerphi(x::PhasedArray1D; f, ϕ, fs=nothing, c=c_0, direction::WaveDirection=Incoming)
     ζ = cos(ϕ)
     ζ = ζ*Int(direction) # propagation direction
     α = ζ/c # slowness vector
@@ -48,7 +48,7 @@ function steerphi(x::PhasedArray1D, f, ϕ; fs=nothing, c=c_0, direction::WaveDir
     return exp.(-1im*uconvert.(NoUnits, Δ*2π*f))
 end
 
-function steerphi(x::PhasedArray2D, f, ϕ; fs=nothing, c=c_0, direction::WaveDirection=Incoming)
+function steerphi(x::PhasedArray2D; f, ϕ, fs=nothing, c=c_0, direction::WaveDirection=Incoming)
     ζ = [cos(ϕ), sin(ϕ)] 
     ζ = ζ*Int(direction) # propagation direction
     α = ζ/c # slowness vector
@@ -59,7 +59,7 @@ function steerphi(x::PhasedArray2D, f, ϕ; fs=nothing, c=c_0, direction::WaveDir
     return exp.(-1im*uconvert.(NoUnits, Δ*2π*f))
 end
 
-function steerphi(x::PhasedArray3D, f, ϕ, θ; fs=nothing, c=c_0, direction::WaveDirection=Incoming)
+function steerphi(x::PhasedArray3D; f, ϕ, θ, fs=nothing, c=c_0, direction::WaveDirection=Incoming)
     ζ = [cos(ϕ)*sin(θ), sin(ϕ)*sin(θ), cos(θ)] 
     ζ = ζ*Int(direction) # propagation direction
     α = ζ/c # slowness vector
@@ -70,16 +70,49 @@ function steerphi(x::PhasedArray3D, f, ϕ, θ; fs=nothing, c=c_0, direction::Wav
     return exp.(-1im*uconvert.(NoUnits, Δ*2π*f))
 end
 
-function dsb_weights(x::PhasedArray1D, f, ϕ; fs=nothing,  c=c_0, direction::WaveDirection=Incoming)
-    return steerphi(x, f, ϕ; fs=fs, c=c, direction=direction)/length(x.elements)
+function steerk(x::PhasedArray1D; f, kx, fs=nothing, c=c_0)
+    k = 2π*f/c # wavenumber
+    ζ = kx/k # propagation direction
+    α = ζ/c # slowness vector
+    Δ = α.*getindex.(x.elements, 1)
+    if(!isnothing(fs))
+        Δ = round.(Δ*fs)/fs
+    end
+    return exp.(-1im*uconvert.(NoUnits, Δ*2π*f))
 end
 
-function dsb_weights(x::PhasedArray2D, f, ϕ; fs=nothing,  c=c_0, direction::WaveDirection=Incoming)
-    return steerphi(x, f, ϕ; fs=fs, c=c, direction=direction)/length(x.elements)
+function steerk(x::PhasedArray2D; f, kx, ky, fs=nothing, c=c_0)
+    k = 2π*f/c # wavenumber
+    ζ = [kx/k, ky/k] # propagation direction
+    α = ζ/c # slowness vector
+    Δ = α[1].*getindex.(x.elements, 1) + α[2].*getindex.(x.elements, 2)
+    if(!isnothing(fs))
+        Δ = round.(Δ*fs)/fs
+    end
+    return exp.(-1im*uconvert.(NoUnits, Δ*2π*f))
 end
 
-function dsb_weights(x::PhasedArray3D, f, ϕ, θ; fs=nothing,  c=c_0, direction::WaveDirection=Incoming)
-    return steerphi(x, f, ϕ, θ; fs=fs, c=c, direction=direction)/length(x.elements)
+function steerk(x::PhasedArray3D; f, kx, ky, kz, fs=nothing, c=c_0)
+    k = 2π*f/c # wavenumber
+    ζ = [kx/k, ky/k, kz/k] # propagation direction
+    α = ζ/c # slowness vector
+    Δ = α[1].*getindex.(x.elements, 1) + α[2].*getindex.(x.elements, 2) + α[3].*getindex.(x.elements, 3)
+    if(!isnothing(fs))
+        Δ = round.(Δ*fs)/fs
+    end
+    return exp.(-1im*uconvert.(NoUnits, Δ*2π*f))
+end
+
+function dsb_weights(x::PhasedArray1D; f, ϕ, fs=nothing,  c=c_0, direction::WaveDirection=Incoming)
+    return steerphi(x, f=f, ϕ=ϕ; fs=fs, c=c, direction=direction)/length(x.elements)
+end
+
+function dsb_weights(x::PhasedArray2D; f, ϕ, fs=nothing,  c=c_0, direction::WaveDirection=Incoming)
+    return steerphi(x, f=f, ϕ=ϕ; fs=fs, c=c, direction=direction)/length(x.elements)
+end
+
+function dsb_weights(x::PhasedArray3D; f, ϕ, θ, fs=nothing,  c=c_0, direction::WaveDirection=Incoming)
+    return steerphi(x, f=f, ϕ=ϕ, θ=θ; fs=fs, c=c, direction=direction)/length(x.elements)
 end
 
 end
