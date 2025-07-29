f605c919c5dcaca890be58c9ffea5f48bb2ea770
scm32482 <matthias.schoen@st.oth-regensburg.de>
2025-07-29 13:37:12 +0200

    SampledArrayManifold: added missing permutations for responses to match sorted axes.

--- src/BeamLib.jl
+++ src/BeamLib.jl
@@ -230,16 +230,33 @@ struct SampledArrayManifold <: ArrayManifold
     itp_phase::AbstractInterpolation
     function SampledArrayManifold(responses::AbstractArray; c_grid=[0], f_grid=[0], coords_grid=AzEl(0))
         num_elements = size(responses)[end]
-        coords_axes = sort.(unique.(eachrow(coords_grid.coords)))
+
+        # sort all axes
+        coords_perms = sortperm.(unique.(eachrow(coords_grid.coords)))
+        coords_axes = map(i -> unique(eachrow(coords_grid.coords)[i])[coords_perms[i]], 1:length(coords_perms))
+
+        c_perms = sortperm(c_grid)
+        c_grid = c_grid[c_perms]
+
+        f_perms = sortperm(f_grid)
+        f_grid = f_grid[f_perms]
+
+        element_perms = Vector(1:num_elements)
+        element_axes = Vector(1:num_elements)
 
         # tuple with all axes of length >1
-        grid_axes = [c_grid, f_grid, coords_axes..., Vector(1:num_elements)]
+        grid_perms = [c_perms, f_perms, coords_perms..., element_perms]
+        grid_axes = [c_grid, f_grid, coords_axes..., element_axes]
         keep_axes = length.(grid_axes) .> 1
+        grid_perms = (grid_perms[keep_axes]...,)
         grid_axes = (grid_axes[keep_axes]...,)
 
-        # resposnes with all axes of length >1
+        # responses with all axes of length >1
         flattened_responses = dropdims(responses; dims=Tuple(findall(size(responses) .<= 1)))
 
+        # permute responsens to match sorted axes again
+        flattened_responses = flattened_responses[grid_perms...]
+        
         itp_mag = extrapolate(interpolate(grid_axes, abs.(flattened_responses), Gridded(Interpolations.Linear())),Interpolations.Flat())
         itp_phase = extrapolate(interpolate(grid_axes, angle.(flattened_responses), Gridded(Interpolations.Linear())),Interpolations.Flat())
         return new(c_grid, f_grid, coords_grid, num_elements, flattened_responses, keep_axes, itp_mag, itp_phase)
