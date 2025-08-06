8a9f10294063c0f8370c08b937378e9d938b593c
scm32482 <matthias.schoen@st.oth-regensburg.de>
2025-08-06 12:45:25 +0200

    lasso() now passes kwargs to proxmal ffb

--- src/BeamLib.jl
+++ src/BeamLib.jl
@@ -874,11 +874,11 @@ References:
 -----------
 Z. Yang, J. Li, P. Stoica, and L. Xie, ‘Sparse methods for direction-of-arrival estimation’, arXiv [cs.IT], 30-Sep-2016.
 """
-function lasso(Y, A, λ=1e-2; maxit=100, tol=1e-6)
+function lasso(Y, A, λ=1e-2; maxit=100, tol=1e-6, kwargs...)
     f = LeastSquares(A, Y)
     g = NormL21(λ, 2)
     X0 = zeros(ComplexF64, size(A,2), size(Y,2))
-    ffb = ProximalAlgorithms.FastForwardBackward(maxit=maxit, tol=tol)
+    ffb = ProximalAlgorithms.FastForwardBackward(maxit=maxit, tol=tol, kwargs...)
     solution, _ = ffb(x0=X0, f=f, g=g)
     return norm.(eachrow(solution), 2).^2
 end
